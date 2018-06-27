0e19147cabe9bcb73806a138c8be2584aa4819dd
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-27 06:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object
 }
 function deleteFromObjectByKey(object, key) {
-  delete object.key
+  var newObject = Object.assign({}, object)
+  delete newobject.key
   return object
 }
